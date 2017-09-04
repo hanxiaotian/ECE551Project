b32432137c656cc9a6dc66d846b36a80995a02ff
xh73 <xh73@duke.edu>
2017-09-04 12:47:47 -0400

    1

---
+++ 019_retirement/retirement.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct _retire_info{
+  int months;
+  double contribution;
+  double rate_of_return;
+}retire_info;
+
+void balanceCompute(int* startAge, double* initial, retire_info info){
+  for(int i=0;i<info.months;i++,(*startAge)++){
+    printf("Age %3d month %2d you have $%.2lf\n",*startAge/12, *startAge%12, *initial);
+    (*initial)+=(*initial)*info.rate_of_return/12+info.contribution;
+  }
+}
+
+void retirement(int startAge, double initial, retire_info working, retire_info retired){
+  balanceCompute(&startAge, &initial, working);
+  balanceCompute(&startAge, &initial, retired);
+}
+
+int main(){
+  int startAge=327;
+  double initial=21345;
+  retire_info working={489,1000,0.045};
+  retire_info retired={384,-4000,0.01};
+  retirement(startAge, initial, working, retired);
+  return 0;
+}
