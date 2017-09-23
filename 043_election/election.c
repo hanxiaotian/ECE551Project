55f426911ba1440da0a299b2272ca8b3d67a3c0e
xh73 <xh73@duke.edu>
2017-09-22 20:41:44 -0400

    1

--- 043_election/election.c
+++ 043_election/election.c
@@ -1,8 +1,39 @@
 #include "election.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 //include any other headers you need here...
 
 state_t parseLine(const char * line) {
   //STEP 1: write me
+  if(strlen(line)==0){
+    fprintf(stderr,"invalid input");
+    exit(EXIT_FAILURE);
+  }
+  char * line1=malloc(strlen(line)*sizeof(char)+1);
+  strcpy(line1,line);
+  state_t state;
+  strcpy(state.name,strtok(line1,":"));
+  for(int i=0;state.name[i]!='\0';i++){
+    if(!isalpha(state.name[i])&&(state.name[i]!=' ')){
+      fprintf(stderr,"invalid state name");
+      exit(EXIT_FAILURE);
+    }
+  }
+  char * ptr;
+  state.population=strtoul(strtok(NULL,":"),&ptr,10);
+  if(state.population<=0){
+    fprintf(stderr,"invalid state population");
+    exit(EXIT_FAILURE);
+  }
+  state.electoralVotes=strtoul(strtok(NULL,":"),&ptr,10);
+  if(state.electoralVotes<=0){
+    fprintf(stderr,"invalid state electoralVotes");
+    exit(EXIT_FAILURE);
+  }
+  free(line1);
+  return state;
 }
 unsigned int countElectoralVotes(state_t * stateData, 
 				 uint64_t * voteCounts, 
