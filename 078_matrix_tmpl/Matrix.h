6db69106bf9f4118b2f37a1cf34acc3cf896f336
xh73 <xh73@duke.edu>
2017-10-13 13:14:55 -0400

    1

--- 078_matrix_tmpl/Matrix.h
+++ 078_matrix_tmpl/Matrix.h
@@ -6,7 +6,78 @@
 #include <vector>
 #include <cstdlib>
 
+using namespace std;
 
-//YOUR CODE GOES HERE!
+template<typename T>
+class Matrix {
+ private:
+  int numRows;
+  int numColumns;
+  vector<vector<T> > mat;
+ public:
+ Matrix():numRows(0),numColumns(0){};
+ Matrix(int r, int c):numRows(r),numColumns(c),mat(r,vector<T>(c)){};
+ Matrix(const Matrix & rhs):numRows(rhs.numRows),numColumns(rhs.numColumns),mat(rhs.mat){};
+ ~Matrix(){};
+ Matrix & operator=(const Matrix & rhs){
+   if(this!=&rhs){
+     numRows=rhs.numRows;
+     numColumns=rhs.numColumns;
+     mat=rhs.mat;
+   }
+   return *this;
+ };
+ int getRows() const{return numRows;};
+ int getColumns() const{return numColumns;};
+ bool operator==(const Matrix & rhs) const{
+   return mat==rhs.mat;
+ };
+ const vector<T> & operator[](int index) const{
+   assert(numRows!=0);
+   assert(numColumns!=0);
+   assert(index>=0);
+   assert(index<numRows);
+   return mat[index];
+ };
+ vector<T> & operator[](int index){
+   assert(numRows!=0);
+   assert(numColumns!=0);
+   assert(index>=0);
+   assert(index<numRows);
+   return mat[index];
+ };
+ Matrix operator+(const Matrix & rhs) const{
+   assert(numRows==rhs.numRows);
+   assert(numColumns==rhs.numColumns);
+   Matrix result(numRows,numColumns);
+   for(int i=0;i<numRows;i++){
+     for(int j=0;j<numColumns;j++){
+       result.mat[i][j]=mat[i][j]+rhs.mat[i][j];
+     }
+   }
+   return result;
+ };
+ friend std::ostream & operator<<(std::ostream & s, const Matrix & rhs){
+   s<<"[ ";
+   for(int i=0;i<rhs.numRows;i++){
+     s<<"{";
+     for(int j=0;j<rhs.numColumns;j++){
+       s<<rhs.mat[i][j];
+       if(j!=rhs.numColumns-1){
+	 s<<", ";
+       }
+     }
+     s<<"}";
+     if(i!=rhs.numRows-1){
+       s<<",\n";
+     }
+   }
+   s<<" ]";
+   return s;
+ };
+
+};
+
+
 
 #endif
