33f60595ffe83a70fa3ce80aee829beea5a78ee8
xh73 <xh73@duke.edu>
2017-10-25 11:12:55 -0400

    1

---
+++ 093_binsrch/search.cpp
@@ -0,0 +1,13 @@
+#ifndef __SEARCH__
+#define __SEARCH__
+
+int binarySearchForZero(Function<int, int> * f, int low, int high){
+  if(low==high || high-low==1) return low;
+  int mid=(low+high)/2;
+  int midvalue=f->invoke(mid);
+  if(midvalue<0) return binarySearchForZero(f,mid,high);
+  if(midvalue>0) return binarySearchForZero(f,low,mid);
+  return mid;
+}
+
+#endif
