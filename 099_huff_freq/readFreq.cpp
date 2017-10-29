cef9d4737aa30d6141416da1bb59c21b9b210106
xh73 <xh73@duke.edu>
2017-10-29 09:02:13 -0400

    1

--- 099_huff_freq/readFreq.cpp
+++ 099_huff_freq/readFreq.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <cstdlib>
 #include "readFreq.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
 
 void printSym(std::ostream & s, unsigned sym) {
   if (sym > 256 ) {
@@ -20,5 +24,14 @@ void printSym(std::ostream & s, unsigned sym) {
   }
 }
 uint64_t * readFrequencies(const char * fname) {
-  //WRITE ME!
+  uint64_t *result=new uint64_t[257]();
+  ifstream file;
+  file.open(fname);
+  int c;
+  while((c=file.get())!=EOF){
+    result[c]++;
+  }
+  if(c==EOF) result[256]++;
+  file.close();
+  return result;
 }
